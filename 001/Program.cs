20f3543c455b642cac06a520c28c1fc4f191062a
Кравченко <sergeiik123@yandex.ru>
2022-11-18 19:40:18 +0300

    Ввод пользователем-вывод квадрата числа

---
+++ 001/Program.cs
@@ -0,0 +1,8 @@
+﻿// 1. С клавиатуры вводится целое число. Вывести квадрат числа.
+
+int a;
+a = 10;
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+int b = a*a;
+System.Console.WriteLine(b);
