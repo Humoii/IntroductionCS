cb6e66daf8914c76c62d587a1b8f33fc52c6e492
Кравченко <sergeiik123@yandex.ru>
2022-11-19 17:34:21 +0300

    введение двух вещественных чисел проверять являеться ли одно квадратом другого

---
+++ 002/Program.cs
@@ -0,0 +1,33 @@
+﻿/* 2. С клавиатуры вводятся два вещественных числа. 
+Проверять является ли одно из них квадратом второго
+*/
+/*
+//полное условие
+if ()
+{
+//true
+}
+else
+{
+//False
+}
+// неполное условие
+if ()
+{
+//True
+}
+*/
+
+System.Console.Write("Введите число a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите число b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+if (a * a == b)
+{
+    System.Console.WriteLine($"{b} являеться квадратом {a}");
+}
+if (b * b == a)
+{
+    System.Console.WriteLine($"{a} являеться квадратом {b}");
+}
