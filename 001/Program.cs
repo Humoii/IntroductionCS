648310522f0c36cdeb565df7649a7836348e6c72
Кравченко <sergeiik123@yandex.ru>
2022-11-19 16:59:38 +0300

    дописалл 001 задачу

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,8 +1,11 @@
 ﻿// 1. С клавиатуры вводится целое число. Вывести квадрат числа.
 
-int a;
+int a;// 32 бит
 a = 10;
+System.Console.Write("Введите число: ");
 string? s = Console.ReadLine();
 a = Convert.ToInt32(s);
 int b = a*a;
-System.Console.WriteLine(b);
+System.Console.WriteLine(a+"^2 = "+b); // "a" склеиваеться спомощью "+" и пишеться строка "^2 = " склеиваем "+" с "b"
+System.Console.WriteLine("{0}^2={1}",a,b);// до интрополяции строк {0}^2={1} -строчка форматирования
+System.Console.WriteLine($"{a}^2={b}"); // $ -интрополяция строк
