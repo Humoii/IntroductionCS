24f0bbfe14349769a8dd20a6dfe3967b6852cc51
Кравченко <sergeiik123@yandex.ru>
2022-11-20 22:30:02 +0300

    Вывести на экран четные числа

---
+++ 009/Program.cs
@@ -0,0 +1,16 @@
+﻿// 9. Вывести на экран четные числа от 1 до N
+
+int N=Convert.ToInt32(Console.ReadLine());
+int i=2;
+while(i<=N)
+{
+    System.Console.Write($"{i} ");// в этом случии после каждого числа, ставяться пробелы
+    i=i+2; // i = i + 1
+}
+
+System.Console.WriteLine();
+
+for(int j=1;j<=N;j=j+2)
+{
+    System.Console.Write($"{j} ");
+}
