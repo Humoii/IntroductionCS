3976fbb2de56a0467654925832165b60b4b6e9d2
Кравченко <sergeiik123@yandex.ru>
2022-11-22 18:18:50 +0300

    BasicAlgorithms

---
+++ BasicAlgorithms/001-Swap/Program.cs
@@ -0,0 +1,28 @@
+﻿// Обмен значениями двух переменныч
+/* Классический способ
+int a=5;
+int b=7;
+int temp;
+temp=a;
+a=b;
+b=temp;
+Console.WriteLine(b);
+Console.WriteLine(a);
+*/
+/* Без использования третьей переменной
+int a=5;
+int b=7;
+a=a+b;
+b=a-b;
+a=a-b;
+Console.WriteLine(b);
+Console.WriteLine(a);
+*/
+/* через "^" -ксор
+ int a=5;
+ int b=7;
+ a=a^b;
+ b=a^b;
+ a=a^b;
+System.Console.WriteLine($"a={a} b={b}");
+*/
