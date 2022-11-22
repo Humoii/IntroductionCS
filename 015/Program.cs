8dd3d85533f42f02699d593cefe2e3c2c136c37e
Кравченко <sergeiik123@yandex.ru>
2022-11-22 17:02:52 +0300

    Вывести третью цифру с начала

---
+++ 015/Program.cs
@@ -0,0 +1,24 @@
+﻿//  15. С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить,
+//что её нет (Вывести: NO).
+
+int n = Convert.ToInt32(Console.ReadLine());
+n=Math.Abs(n); // Abs -обсолютное значение
+/* Вариант 1-простой
+if (n<100) System.Console.WriteLine("No");
+else System.Console.WriteLine(n/100%10);
+*/
+
+// Вариант 2 - 12345678 - третье число с начала
+
+if (n > 999)
+{
+    while (n > 1000)
+    {
+        n = n / 10;
+    }
+    System.Console.WriteLine(n % 10);
+}
+else
+{
+    System.Console.WriteLine("NO");
+}
