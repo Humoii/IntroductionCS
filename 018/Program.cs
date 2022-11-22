28761d509809be3c929ea21ad9a3ceace16aa14c
Кравченко <sergeiik123@yandex.ru>
2022-11-22 18:43:51 +0300

    является ли одно квадратом другого

---
+++ 018/Program.cs
@@ -0,0 +1,9 @@
+﻿// 18. По двум заданным числам проверять является ли одно квадратом другого.
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b) System.Console.WriteLine($"{b} являеться квадратом {a}");
+    else
+        if (b * b == a) System.Console.WriteLine($"{a} являеться квадратом {b}");
+            else
+                System.Console.WriteLine("Не являеться");
