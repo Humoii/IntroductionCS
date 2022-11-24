db63948df15e891be71e7d8b3b87a93f2e0fecef
Сергей <sergeiik123@yandex.ru>
2022-11-24 19:35:49 +0300

    По заданному с клавиатуры номеру дня недели вывести его название

--- 004/Program.cs
+++ 004/Program.cs
@@ -5,27 +5,27 @@ if (a==1)
 {
     System.Console.WriteLine("Понедельник");
 }
-if (a==2)
+else if(a==2)
 {
     System.Console.WriteLine("Вторник");
 }
-if (a==3)
+else if (a==3)
 {
     System.Console.WriteLine("Среда");
 }
-if (a==4)
+else if (a==4)
 {
     System.Console.WriteLine("Четверг");
 }
-if (a==5)
+else if (a==5)
 {
     System.Console.WriteLine("Пятница");
 }
-if (a==6)
+else if (a==6)
 {
     System.Console.WriteLine("Суббота");
 }
-if (a==7)
+else if (a==7)
 {
     System.Console.WriteLine("Воскресение");
 }
