4c6442995093e2a3d4e715919d892288e71cd990
Сергей <sergeiik123@yandex.ru>
2022-11-25 21:23:54 +0300

    Вывести на экран кубы чисел от 1 до N

---
+++ 025/Program.cs
@@ -0,0 +1,17 @@
+﻿// 25. Вывести на экран кубы чисел от 1 до N
+void CubesOfNumbers(int Number)
+
+{
+    int i = 1;
+    int b = 0;
+    while (i <= Number)
+    {
+        b = (int)Math.Pow(i, 3);
+        i++;
+        Console.WriteLine(b);
+    }
+    
+}
+System.Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+CubesOfNumbers(n);
