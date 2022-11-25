634a431609c211138dba628e8ff2330e923542a1
Сергей <sergeiik123@yandex.ru>
2022-11-25 16:19:10 +0300

    добавили методы и использовали ref и out

--- BasicAlgorithms/001-Swap/Program.cs
+++ BasicAlgorithms/001-Swap/Program.cs
@@ -26,3 +26,29 @@ Console.WriteLine(a);
  a=a^b;
 System.Console.WriteLine($"a={a} b={b}");
 */
+/* через метод(подпрограмму)
+void Swap(ref int a, ref int b) //ref(reference)-ссылка если надо менять переменную в нутри(передача данных по ссылке) не создаеться копия a и b а переводит по ссылке где они лежат в памяти
+{
+    int t = a;
+    a = b;
+    b = t;
+}
+int a=3, b=5;
+
+System.Console.WriteLine($"a={a} b={b}");
+Swap(ref a, ref b); // подпрограмма обязательно вписуем ref если его используем
+System.Console.WriteLine($"a={a} b={b}");
+*/
+
+//void Calculator(int a, int b, ref int sum, ref int multi) // ref используеться для множества значений, через int -можем вернуть только одно значение
+void Calculator(int a, int b, out int sum, out int multi)//out -также используеться как ref только не обязательно передавать значения int sum,multi;
+{
+    sum=a+b;
+    multi=a*b;
+}
+int a=3, b=5;
+// int sum=0, multi=0;
+// Calculator(a,b,ref sum,ref multi); //для ref обязательно передача значения int sum=0, multi=0;
+int sum,multi;
+Calculator(a,b,out sum,out multi); // для out не обязательна передача значения int sum,multi;
+System.Console.WriteLine($"Сумма={sum} Умножение={multi}");
