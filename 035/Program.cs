55fbbb22c459ed3e042e0e863476724ed2134c98
Сергей <sergeiik123@yandex.ru>
2022-11-26 19:09:31 +0300

    замена элементов массива на противоположные

---
+++ 035/Program.cs
@@ -0,0 +1,14 @@
+﻿// 35. Написать программу замены элементов массива на противоположные
+
+int[] array = {1,-2,3,-4,5,0};
+
+int index =0;
+int b =0;
+while (index < array.Length)
+{
+    b = -1 * array[index];
+
+    index++;
+    Console.WriteLine(b);
+}
+
