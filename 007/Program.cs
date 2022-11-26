ff8fccb766863e24461e88da73dcd01888db7f73
Сергей <sergeiik123@yandex.ru>
2022-11-26 19:05:19 +0300

    Выяснить является ли число чётным.

--- 007/Program.cs
+++ 007/Program.cs
@@ -1,2 +1,8 @@
-﻿// Выяснить является ли число чётным.
+﻿// 7. Выяснить является ли число чётным.
+
+int a = Convert.ToInt32(Console.ReadLine());
+bool flag=false;
+
+if (a % 2 == 0) flag=true;
+if (flag) Console.WriteLine("Четное"); else System.Console.WriteLine("Не четное");
 
