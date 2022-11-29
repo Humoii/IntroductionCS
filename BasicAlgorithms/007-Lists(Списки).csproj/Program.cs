7625a944f99bc7c7e2ed161c7357f0345bc9e0c7
Сергей <sergeiik123@yandex.ru>
2022-11-29 12:27:07 +0300

    Списки

---
+++ BasicAlgorithms/007-Lists(Списки).csproj/Program.cs
@@ -0,0 +1,14 @@
+﻿// Списки (списки работают дольше массивов)
+
+List<int> temp;
+temp=new List<int>();
+for(int i=0;i<7;i++)
+{
+    temp.Add (Convert.ToInt32(Console.ReadLine()));
+}
+double sum=0;
+for(int i=0; i<7; i++)
+{
+    sum=sum+temp[i];
+}
+System.Console.WriteLine(sum/temp.Count); // Count -счетчик элементов списка
