f8b1c2de15699269fca4b618fd05f079e0286d32
Сергей <sergeiik123@yandex.ru>
2022-11-29 12:26:13 +0300

    Тренеровочная

---
+++ 000/Program.cs
@@ -0,0 +1,11 @@
+﻿// тренеровочная
+
+double[] a = new double[5];
+ 
+Random random = new Random();
+    for (int j = 0; j < 5; j++)
+    {
+        a[j] = random.Next(100);
+        Console.Write("{0,4}", a[j]);
+    }
+    Console.WriteLine();
