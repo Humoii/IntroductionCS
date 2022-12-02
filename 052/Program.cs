f6a0d9bb554fa263fba037e9e66e7911975e32df
Сергей <sergeiik123@yandex.ru>
2022-12-02 16:18:14 +0300

    Написать программу преобразования десятичного числа в двоичное

---
+++ 052/Program.cs
@@ -0,0 +1,17 @@
+﻿// 52. Написать программу преобразования десятичного числа в двоичное
+
+string DecimalToBin(int n)
+{
+    string s="";
+    if (n==0) return "0";
+    while(n!=0)
+{
+    // System.Console.WriteLine(n%2);
+    s=Convert.ToString(n%2)+s;
+    n/=2;
+}
+return s;
+}
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+System.Console.WriteLine(DecimalToBin(n));
