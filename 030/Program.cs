0c6bf1471de18650cb60803dcb765c9af46b56c0
Сергей <sergeiik123@yandex.ru>
2022-12-02 16:19:06 +0300

    Вывести на экран таблицу квадратов чисел от 1 до N

---
+++ 030/Program.cs
@@ -0,0 +1,19 @@
+﻿// 30. Вывести на экран таблицу квадратов чисел от 1 до N
+
+int NumberSquaresTabl(int Number)
+{
+    
+    for (int i=1;i <= Number;i++)
+    {
+        System.Console.WriteLine($"{i,4}{i*i,10}");
+    }
+    return Number;
+}
+
+System.Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+NumberSquaresTabl(N);
+
+// int n=10;
+// for(int i=0;i<n;i++)
+// System.Console.WriteLine($"{i,4}{i*i,10}");
