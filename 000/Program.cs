d9d5f062b035012b748aabb6fbb7cc4de706378c
Сергей <sergeiik123@yandex.ru>
2022-12-02 16:20:01 +0300

    Тренеровочная

--- 000/Program.cs
+++ 000/Program.cs
@@ -1,48 +1,16 @@
 ﻿// тренеровочная
-// Создание вещественного массива
-// double[] a = new double[3];
- 
-// Random random = new Random();
-//     for (int j = 0; j < 3; j++)
-//     {
-//         a[j] = random.NextDouble();
-//         Console.Write($"{a[j],4:F1}");
-//     }
-//     Console.WriteLine();
-double[] RandomIntArray(int size = 0)
-{
-    double[] array = new double[size];
-    Random random = new Random();
 
-    for (int index = 0; index < size; index++)
-        array[index] = random.NextDouble();
-    
-    return array;
+int NumberSquares(int Number)
+{
+
+    for (int i=0;i < Number;i++)
+    {
+        Number = i * i;
+        Console.WriteLine(Number);
+    }
+    return Number;
 }
 
-double Max(double[] array)
-{
-    double max = array[0];
-    for (int index = 1; index < array.Length; index++)
-        if (array[index] > max) max = array[index];
-    return max;
-}
-
-double Min(double[] array)
-{
-    double min = array[0];
-    for (int index = 1; index < array.Length; index++)
-        if (array[index] < min) min = array[index];
-    return min;
-}
-
-void Print(double[] array)
-{
-    for (int index = 0; index < array.Length; index++)
-        System.Console.Write($"{array[index],5:F2}");
-}
-double[] a = RandomIntArray(5);
-Print(a);
-
-System.Console.WriteLine();
-System.Console.WriteLine(Max(a) - Min(a));
+System.Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NumberSquares(N));
