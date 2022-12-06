938866f1c9555871649398ac650b0c932916a3c7
Сергей <sergeiik123@yandex.ru>
2022-12-06 16:38:02 +0300

    Написать программу копирования массива

---
+++ 056/Program.cs
@@ -0,0 +1,39 @@
+﻿// 56. Написать программу копирования массива
+// int[] ArrayRandom(int n)
+// {
+//   int[] array = new int [n];
+//   Random random = new Random();
+//   for (int i=0;i<array.Length;i++)
+//   {
+//   array[i]= random.Next(0,200);
+//   }
+//   return array;
+// }
+int[] CopyArray (int [] array)
+{
+  int[] copy = new int[array.Length];
+for (int i=0;i<array.Length;i++)
+{
+  copy[i] = array[i];
+}
+return copy;
+}
+void Print(string n, int[] array)
+{
+  Console.Write(n);
+    for (int index = 0; index < array.Length; index++)
+        {
+        System.Console.Write($"{array[index],5}");
+        }
+}
+// int n = Convert.ToInt32(Console.ReadLine());
+// int [] N = ArrayRandom(n);
+// Copy(N);
+ int[] array = { 5, 4, 7, 2, 9 };
+Print($"Array:",array);
+Console.WriteLine();
+Print($"Copy array:",CopyArray(array));
+Console.WriteLine();
+int[] n = CopyArray(array);
+n[1]=11;
+Print($"Copy array:",n);
