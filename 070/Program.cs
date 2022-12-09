78b28eb319a4f78cfbd8f18d9dfb395e1a558df9
Сергей <sergeiik123@yandex.ru>
2022-12-09 15:01:19 +0300

    Показать натуральные числа от 1 до N, N задано

---
+++ 070/Program.cs
@@ -0,0 +1,15 @@
+﻿// 70. Показать натуральные числа от 1 до N, N задано
+
+
+void Loop(int N,int i = 1)
+{
+
+    if(i<=N)
+    {
+        System.Console.Write($"{i,5}");
+        Loop(N,i+1);
+    }
+    
+}
+
+Loop(10); 
