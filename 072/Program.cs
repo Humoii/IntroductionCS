f4ccb64380e1ae0830367dfe95ca519c18a62f2c
Сергей <sergeiik123@yandex.ru>
2022-12-09 15:02:07 +0300

    Показать натуральные числа от M до N, N и M заданы

---
+++ 072/Program.cs
@@ -0,0 +1,15 @@
+﻿// 72. Показать натуральные числа от M до N, N и M заданы
+
+
+void Loop(int M,int N)
+{
+
+    if(M<=N)
+    {
+        System.Console.Write($"{M,5}");
+        Loop(M+1,N);
+    }
+    
+}
+
+Loop(5,10); 
