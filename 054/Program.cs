c5557f911a52ea98ad3e6f29bc6f22fc2406452c
Сергей <sergeiik123@yandex.ru>
2022-12-10 13:11:40 +0300

    С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1

--- 054/Program.cs
+++ 054/Program.cs
@@ -1,23 +1,23 @@
 ﻿// 54. С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. 
 // Принять первые числа равными 0 и 1
-void Fibonacci(int n)
-{
-    int a = 1;
-    int b = 0;
-    int c = 0;
-    Console.Write(0);
-    for (int i = 0; i < n - 1; i++)
-    {
-        c = a + b;
-        a = b;
-        b = c;
+// void Fibonacci(int n)
+// {
+//     int a = 1;
+//     int b = 0;
+//     int c = 0;
+//     Console.Write(0);
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         c = a + b;
+//         a = b;
+//         b = c;
 
-        Console.Write($" {c}");
-    }
-}
+//         Console.Write($" {c}");
+//     }
+// }
 
 int n = Convert.ToInt32(Console.ReadLine());
-Fibonacci(n);
+// Fibonacci(n);
 
 //через массив
 
@@ -28,4 +28,4 @@ a[1] = 1;
 for (int i = 2; i < n; i++)
     a[i] = a[i - 1] + a[i - 2];// рекурентная формула (следующее значение за счет использования предыдущих)
 for (int i = 0; i < n; i++)
-    System.Console.Write($"{a[i]}");
+    System.Console.Write($"{a[i],5}");
