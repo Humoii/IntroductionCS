70aa8011011b4c8cbb85cf7bc0a6de9a732e5c66
Сергей <sergeiik123@yandex.ru>
2022-12-11 15:22:45 +0300

    Подсчитать количество цифр среди вводимых с клавиатуры символов

---
+++ 083/Program.cs
@@ -0,0 +1,14 @@
+﻿// 83. Подсчитать количество цифр среди вводимых с клавиатуры символов
+
+
+string s = Console.ReadLine()!;// ! -Добавлен оператор для прощения значений NULL в правую часть назначения.
+int k = 0;
+// for (int i = 0; i < s.Length; i++)
+// {
+//     if(char.IsDigit(s[i])) k++;
+// }
+foreach (char c in s)
+{
+    if (char.IsDigit(c)) k++;// IsDigit -указывает находиться ли указаный символ как десятичная цифра
+}
+System.Console.WriteLine(k);
