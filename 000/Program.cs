44caecd505a7d38879934643ee0924fa28d009f2
Сергей <sergeiik123@yandex.ru>
2022-12-11 15:26:00 +0300

    Проба

--- 000/Program.cs
+++ 000/Program.cs
@@ -1,16 +1,2 @@
 ﻿// тренеровочная
 
-int NumberSquares(int Number)
-{
-
-    for (int i=0;i < Number;i++)
-    {
-        Number = i * i;
-        Console.WriteLine(Number);
-    }
-    return Number;
-}
-
-System.Console.Write("Введите число: ");
-int N = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(NumberSquares(N));
