57ed74c394dd82cdc06815b2a90b7f2b26c4da4b
Сергей <sergeiik123@yandex.ru>
2022-12-11 15:23:36 +0300

    Удалить среднюю букву при нечетное длине строки и две средние буквы при четной длине строки

---
+++ 087/Program.cs
@@ -0,0 +1,22 @@
+﻿// 87. Удалить среднюю букву при нечетное длине строки 
+// и две средние буквы при четной длине строки
+
+ string s=Console.ReadLine()!;
+ string s1;
+int m=s.Length/2;
+ if (s.Length%2==0)
+ {
+   s1=s.Remove(m-1,2);//Remove -позволяет удалить символ
+ System.Console.WriteLine(s1);
+ if(m==1)
+
+    System.Console.WriteLine(s);
+}
+else
+{
+    s1=s.Remove(m,1);
+System.Console.WriteLine(s1);
+if(m==0)
+
+    System.Console.WriteLine(s);
+}
