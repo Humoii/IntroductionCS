931e0c594fcaf64d837b799f2f4584f49fa6b0e5
Сергей <sergeiik123@yandex.ru>
2022-12-11 15:24:59 +0300

    Строки

---
+++ BasicAlgorithms/022-Строки/Program.cs
@@ -0,0 +1,21 @@
+﻿// Строки
+/* Для ввода нескольких значений через строку 10 20 30
+
+string[] s = Console.ReadLine();
+string[] ss = s.Split(' ');
+int[] n = new int[ss.Length];
+for (int i = 0; i < ss.Length; i++)
+{
+    n[i] = Convert.ToInt32(ss[i]);
+}
+int a = n[0];
+int b = n[1];
+int c = n[2];
+*/
+
+string[] ss = Console.ReadLine()!.Split(' ');// ! -Добавлен оператор для прощения значений NULL в правую часть назначения.
+int[] n = Array.ConvertAll<string,int>(ss,Convert.ToInt32);// обобщенные типы и параметризированные
+int a = n[0];
+int b = n[1];
+int c = n[2];
+
