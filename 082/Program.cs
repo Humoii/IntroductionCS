3afba8c69346b2a55be36ebd5b05c2027f41afba
Сергей <sergeiik123@yandex.ru>
2022-12-12 19:50:01 +0300

    C клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки

---
+++ 082/Program.cs
@@ -0,0 +1,15 @@
+﻿// 82. C клавиатуры вводится строка разделенная точкой.
+// Подсчитать количество символов до точки
+
+string s = Console.ReadLine()!;
+int k = 0;
+if (k < s.IndexOf('.'))
+{
+    for (int i = 0; i < s.IndexOf('.'); i++)
+    {
+        k++;
+    }
+    System.Console.WriteLine(k);
+}
+else
+    System.Console.WriteLine("No separator");
