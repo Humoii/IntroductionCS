5a9b8aade9712e82dd11926e21960a138cd8f038
Сергей <sergeiik123@yandex.ru>
2022-12-12 19:51:34 +0300

    Сколько 'a' в строке

---
+++ 086/Program.cs
@@ -0,0 +1,15 @@
+﻿// 86. Подсчитать сколько раз определенный символ (например 'a') встречается в строке.
+
+
+
+string s = Console.ReadLine()!;
+
+char find = 'a';
+int count = 0;
+foreach (var el in s)
+{
+    if (el == find)
+        count++;
+}
+System.Console.WriteLine();
+System.Console.WriteLine(count);
