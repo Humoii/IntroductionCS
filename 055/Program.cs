5b0aece41e15c30246ceaa33114de22a95c5d83a
Сергей <sergeiik123@yandex.ru>
2022-12-16 17:38:44 +0300

    Написать программу масштабирования фигуры

--- 055/Program.cs
+++ 055/Program.cs
@@ -11,7 +11,19 @@ class Program
     struct Point
     {
         public double x, y;
+
+        public Point(double _x,double _y)
+        {
+            x=_x;
+            y=_y;
+        }
     }
+
+     static double Distance(double x1, double y1, double x2, double y2)
+    {
+        return Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2));
+    }
+
     static double Distance(Point A, Point B)
     {
         return Math.Sqrt(Math.Pow(A.x-B.x,2)+Math.Pow(A.y-B.y,2));
@@ -27,6 +39,10 @@ class Program
         System.Console.WriteLine(Distance(a,b));
 
         Point[] figure = new Point[3];
+        figure[0]=new Point(0,0);
+        figure[1]=new Point(1,1);
+        figure[2]=new Point(0,1);
+        
         double k=2;
         for(int i=0;i<figure.Length;i++)
         {
@@ -34,4 +50,7 @@ class Program
             figure[i].y=figure[i].y*k;
         }
     }
+
+
+    
 }
