9ada82e38fc7ed5889d241aad836425621fc10cf
Сергей <sergeiik123@yandex.ru>
2022-12-16 17:39:45 +0300

    Написать программу, упорядочивания по убыванию элементы каждой строки двумерного массива.

--- 069/Program.cs
+++ 069/Program.cs
@@ -13,14 +13,14 @@ int[,] Random2DArray(int n, int m, int min = 0, int max = 30)
 void Sort2DArray(int[,] a)
 {
     for (int i = 0; i < a.GetLength(0); i++)
-        for (int k = 0; k < a.GetLength(1); k++)
+        for (int k = 0; k < a.GetLength(1); k++) 
             for (int j = 0; j < a.GetLength(1) - 1; j++)
-                if (a[i, j] < a[i, j+1])
+                if (a[i, j] > a[i, j+1])
                 {
                     int t = a[i, j];
                     a[i, j] = a[i, j+1];
                     a[i, j+1] = t;
-                }
+                } 
 }
 
 void Print2DArray(int[,] a)
@@ -34,5 +34,7 @@ void Print2DArray(int[,] a)
 }
 
 int[,] a = Random2DArray(3, 5);// (3,5) - "3" количество строк - "5" количество столбцов
+Print2DArray(a);
+System.Console.WriteLine();
 Sort2DArray(a);
 Print2DArray(a);
