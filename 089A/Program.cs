c0def04407a4b7dd163d724d7359da0e45faa748
Сергей <sergeiik123@yandex.ru>
2022-12-16 17:41:40 +0300

    Построение словоря из букв

---
+++ 089A/Program.cs
@@ -0,0 +1,34 @@
+﻿// 89A. ***В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». 
+// Покажите все слова (слова не обязательно осмысленные), состоящие из n букв,
+//  которые можно построить из букв этого алфавита. Здесь используется рекурсия.
+
+
+// string s="аисв";
+// foreach (var c1 in s)
+//     foreach (var c2 in s)
+//     {
+//     System.Console.Write($"{c1},{c2}");
+//     }
+
+
+
+
+void Words(string A, ref string w, int N)
+{
+if (N < 1)
+{
+Console.Write($"{w} ");
+return;
+}
+foreach (char c in A)
+{
+w=w+c;
+Words(A,ref w, N-1);
+w = w.Remove(w.Length - 1);
+}
+}
+
+Console.Write("Введите количество букв, из которых будет состоять слово: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string word = "";
+Words("аисв", ref word, n);
