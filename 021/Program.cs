f2a7a40c754054f0943e4778a280683feca8eb6c
Сергей <sergeiik123@yandex.ru>
2022-12-20 14:06:15 +0300

    Дано число обозначающее день недели. Выяснить является номер дня недели выходным

---
+++ 021/Program.cs
@@ -0,0 +1,24 @@
+﻿// 21. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+
+string[] DaysOfTheWeek = {"NO", "Понедельник", "Вторник", "Среда", "Четверг",
+                            "Пятница", "Суббота", "Воскресение"};
+System.Console.Write("Введите день недели: ");
+int index = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(DaysOfTheWeek[index]);
+if (index == 0)
+{
+    System.Console.WriteLine();
+}
+else
+{
+    if (index == 5 || index == 6)
+    {
+        System.Console.WriteLine("Выходной");
+    }
+    else
+    {
+        System.Console.WriteLine("Рабочий");
+    }
+}
+
