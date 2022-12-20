8ab3733542628c6a01126805760750b90ed96787
Сергей <sergeiik123@yandex.ru>
2022-12-20 14:07:52 +0300

    Подсчитать сумму цифр в числе. Сделать подпрограмму.

---
+++ 035/Program.cs
@@ -0,0 +1,18 @@
+﻿// 35. Подсчитать сумму цифр в числе. Сделать подпрограмму.
+
+int SumOfDigits(int n)
+{
+    int k = 0;
+    int sum = 0;
+    for (int i = 0; i < n; i = i / 10)
+    {
+        k = n % 10;
+        n = n / 10;
+        sum = sum + k;
+    }
+    return sum;
+}
+
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(SumOfDigits(a));
+
