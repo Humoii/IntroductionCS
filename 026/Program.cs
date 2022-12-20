371ad181c5a33528d1f157bcfd78df90b48843b5
Сергей <sergeiik123@yandex.ru>
2022-12-20 14:06:57 +0300

    Программа проверяет пятизначное число на палиндром

---
+++ 026/Program.cs
@@ -0,0 +1,17 @@
+﻿// 26. Программа проверяет пятизначное число на палиндром.
+
+int n = 12321;
+int c = n;
+int f = 0;
+int sum = 0;
+for (int i = 0; i < n; i=i/10)
+{
+    f = n % 10;
+    n = n / 10;
+    sum = sum*10+f;
+}
+System.Console.WriteLine(sum);
+bool d1=sum==c;
+System.Console.WriteLine(d1);
+
+
