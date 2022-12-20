13bcabe5defbdc064545dc1d62177a761fa15fdc
Сергей <sergeiik123@yandex.ru>
2022-12-20 14:08:10 +0300

    Написать программу вычисления произведения чисел от 1 до N

---
+++ 036/Program.cs
@@ -0,0 +1,17 @@
+﻿// 36. Написать программу вычисления произведения чисел от 1 до N
+
+int ProductOfNumbers(int n)
+{
+    int k = 0;
+    int sum = 1;
+    for (int i = 0; i < n; i++)
+    {
+        k = k + 1;
+        sum = sum * k;
+
+    }
+    return sum;
+}
+
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(ProductOfNumbers(a));
