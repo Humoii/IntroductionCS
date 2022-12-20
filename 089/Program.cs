c7def73761cdcb7d6e5751d0e670d3ce832d5f30
Сергей <sergeiik123@yandex.ru>
2022-12-20 14:09:38 +0300

    Подсчитать сумму цифр, встречающихся в строке

--- 089/Program.cs
+++ 089/Program.cs
@@ -1,3 +1,20 @@
 ﻿// 89. Подсчитать сумму цифр, встречающихся в строке
 
 
+
+string s = "1f234f5";
+
+int SumDigits(string s)
+{
+    int sum = 0;
+    for(int i = 0; i < s.Length; i++)
+    {
+        if ((s[i] > '0') && (s[i] < '9'))
+        {
+            sum = sum + (s[i] - '0');
+        }
+    }
+    return sum;
+}
+
+System.Console.WriteLine(SumDigits(s));
