1547556694b7bf5efc2f9aac1faabc322c1daa09
Сергей <sergeiik123@yandex.ru>
2022-12-20 14:07:36 +0300

    Возведите число А в натуральную степень B используя цикл

---
+++ 033/Program.cs
@@ -0,0 +1,15 @@
+﻿// 33. Возведите число А в натуральную степень B используя цикл
+
+int NaturalDegree(int a, int b)
+{
+int j = 1;
+for (int i = 0; i < b; i++)
+{
+    j = j*a;
+}
+return j;
+}
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(NaturalDegree(a,b));
