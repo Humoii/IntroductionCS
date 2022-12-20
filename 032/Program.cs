f06ec16319092b7187bf20889ddfcc67c7e758f5
Сергей <sergeiik123@yandex.ru>
2022-12-20 14:07:18 +0300

    Найти сумму чисел от 1 до А

---
+++ 032/Program.cs
@@ -0,0 +1,18 @@
+﻿// 32. Найти сумму чисел от 1 до А
+
+
+int SummaNum(int n)
+{
+    int k = 0;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        k = k + 1;
+        sum = sum + k;
+
+    }
+    return sum;
+}
+
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(SummaNum(a));
